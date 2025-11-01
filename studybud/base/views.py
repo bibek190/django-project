ab4109388c5649d8e8e96dbb9c22b38a431de5cb
bibek190 <bibek9818917036@gmail.com>
2025-11-01 14:33:24 +1100

    add crud

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -22,11 +22,33 @@ def room(request,pk):
 
 def createRoom(request):
     form = RoomForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home') 
+    
+    
+    context = {'form':form}
+    return render(request,'base/room_form.html',context)
+
+
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    
+    if request.method == "POST":
+        form = RoomForm(request.POST,instance=room)
         if form.is_valid():
             form.save()
             return redirect("home")
     
-    context = {'form':form}
-    return render(request,'base/room_form.html',context)
+    context = {"form": form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == "POST":
+        room.delete()
+        return redirect('home')
+    return render(request,'base/deleteRoom.html',{'obj':room})
